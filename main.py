bfdff50bc4e523ee63abc7a509d682447c6f1f95
N-Hiro0123 <n.hiroki0909@gmail.com>
2024-08-26 00:46:13 +0900

    環境変数の読み込み方法の確認

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 
+import os
+from dotenv import load_dotenv
+
+# 環境変数のロード
+load_dotenv()  # デプロイ時に残しておいても問題ないらしい（あやしければ無効にする）
+TEST_SECRET = os.getenv("TEST_SECRET")
+
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"message": "Hellow World"}
+    return {"message": TEST_SECRET}
